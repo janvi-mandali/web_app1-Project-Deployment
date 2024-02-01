594d4448239180d46772825da6cd15b2ebcc2b5d
janvi-mandali <mandalimandalijanvi@gmail.com>
2024-02-01 11:44:31 +0530

    Web_py: Project deployment

---
+++ functions.py
@@ -0,0 +1,19 @@
+def get_todos(filepath="todos.txt"):
+    """
+    Read a text file and return the list of 
+    to-do items.
+    
+    """
+    with open(filepath,'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg,filepath="todos.txt"):
+    """write the to-do item list in the text files."""
+    with open(filepath,'w')as file:
+        file.writelines(todos_arg)
+        
+if __name__== '__main__':
+    print("hello")
+    print(get_todos())
+        
